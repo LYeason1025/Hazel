1fec27a843e75ddea1bf77c8651cd2634308e284
iverson <321682959@qq.com>
2023-06-19 15:06:05 +0800

    Setup basic Application and Entry point

---
+++ Sandbox/src/SanboxApp.cpp
@@ -0,0 +1,23 @@
+#include <Hazel.h>
+
+class Sandbox:public Hazel::Application
+{
+public:
+	Sandbox();
+	~Sandbox();
+
+private:
+
+};
+
+Sandbox::Sandbox()
+{
+}
+
+Sandbox::~Sandbox()
+{
+}
+
+Hazel::Application*Hazel::CreateApplicaton() {
+	return new Sandbox();
+}
